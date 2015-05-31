796b6f49450b65b701691ee21fa33140f854292f
Mario Visic <mario@mariovisic.com>
2015-05-31 21:00:55 +1000

    Add first bits of testing

---
+++ spec/controllers/activities_controller_spec.rb
@@ -0,0 +1,3 @@
+require 'rails_helper'
+RSpec.describe ActivitiesController, type: :controller do
+end
---
+++ spec/features/frontend_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.feature 'Viewing the front end' do
+  before do
+    Activity.create!(title: 'Super hard session', body: ['It was a really super hard session', ('hello ' * 1000), 'I promise'].join)
+  end
+
+  scenario 'showing a list of recent activities' do
+    visit '/'
+
+    expect(page).to have_content('Super hard session')
+    expect(page).to have_content('It was a really super hard session')
+  end
+
+  scenario 'truncating activities on the homepage' do
+    visit '/'
+
+    expect(page).to have_content('It was a really super hard session')
+    expect(page).to have_no_content('I promise')
+
+    click_link 'Read more...'
+
+    expect(page).to have_content('I promise')
+  end
+end
